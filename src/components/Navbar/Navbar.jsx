148c6fd289cc5af4d3780e31e8c2773bf2cc51b8
Akash Meher <akashmeherofficial@gmail.com>
2022-07-28 19:00:12 +0530

    second commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,8 +13,8 @@ const Navbar = () => {
        <div className="rightnav">
             <ul className='nav'>
                 <li ><NavLink to="/" className="navitem">Home</NavLink></li>
-                <li ><NavLink to="/about" className="navitem">About</NavLink></li>
                 <li ><NavLink to="/products" className="navitem">Products</NavLink></li>
+                <li ><NavLink to="/about" className="navitem">About</NavLink></li>
             </ul>
                 <button className="navitembtn"><NavLink to="/contact" className='contactbtn'>Contact Us</NavLink></button>
        </div>
